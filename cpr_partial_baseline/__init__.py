a7493d8800aeb0dc7e0820633111b9822ff1b7ef
ejung11 <ejung11@student.gsu.edu>
2022-04-09 15:16:20 -0400

    Fixed!! 'Baseline' working well. Now, lets test with more people and rounds.

--- cpr_partial_baseline/__init__.py
+++ cpr_partial_baseline/__init__.py
@@ -31,7 +31,13 @@ class Group(BaseGroup):
     #need to implement function
     destruction = models.IntegerField()
     end = models.BooleanField(
-        initial = False, doc = """Indicates whether the game will continue or end"""
+        initial = False, doc = """Indicates whether the game will continue or end. Stay True after triggered"""
+    )
+    end_temp = models.BooleanField(
+        initial = False, doc = """Turn True only for the round"""
+    )
+    end_prev = models.BooleanField(
+        initial = False, doc = """Stay True after triggered but +1 round """
     )
 
 
@@ -42,7 +48,13 @@ class Player(BasePlayer):
     history_accumulated_earnings = models.FloatField()
     period_payoff = models.FloatField()
     end = models.BooleanField(
-        initial = False, doc = """Indicates whether the game will continue or end"""
+        initial = False, doc = """Indicates whether the game will continue or end. Stay True after triggered"""
+    )
+    end_temp = models.BooleanField(
+        initial = False, doc = """Turn True only for the round"""
+    )
+    end_prev = models.BooleanField(
+        initial = False, doc = """Stay True after triggered but +1 round """
     )
 
 
@@ -84,25 +96,23 @@ def set_payoffs(g: Group):
         g.prob_ending = round((g.total_harvest / (Constants.endowment * Constants.players_per_group)) * 100)
         print('prob of ending', g.prob_ending)
 
-        # end or continue?(Prof's recommendation)
+        # end or continue?
+        # var 'end' == False initially but stay True when triggered
         if g.destruction <= g.total_harvest:
             for group_to_modify in g.in_rounds(g.round_number, Constants.num_rounds):
                 group_to_modify.end = True
                 print('end variable in round', group_to_modify.round_number, group_to_modify.end)
 
-        #end or continue?
-        # if g.destruction <= g.total_harvest:
-        #     p.group.end = True
-        # else:
-        #     p.group.end = False
-        # print('end?', g.destruction, p.group.end)
+            # Create end_prev var to indicate previous 'end' state
+            for group_to_modify in g.in_rounds(g.round_number+1, Constants.num_rounds):
+                group_to_modify.end_prev = True
 
-        #testing the idea of getting previous value of p.group.end
-        # if p.round_number == 1:
-        #     prev_player = p.round_number(1)
-        # else:
-        #     prev_player = p.in_round(p.round_number - 1)
-        #     print('prev end?', prev_player.group.end)
+        # Create temporary var that will only be True for the destruction round. and stay False
+        if g.destruction <= g.total_harvest:
+            p.group.end_temp = True
+        else:
+            p.group.end_temp = False
+        print('end_tepm?', g.destruction, p.group.end_temp)
 
     #Earnings for each round
     for p in g.get_players():
@@ -130,19 +140,31 @@ class Harvest(Page):
     form_model = 'player'
     form_fields = ['harvest']
 
+    @staticmethod
+    def is_displayed(player):
+        return player.group.end == False
+
 
 class ResultsWaitPage(WaitPage):
     after_all_players_arrive = 'set_payoffs'
+
+    @staticmethod
+    def is_displayed(player):
+        return player.group.end_prev == False
 
 
 class Results(Page):
     """Players payoff: How much each has earned"""
 
+    @staticmethod
+    def is_displayed(player):
+        return player.group.end_prev == False
+
 
 class Destruction(Page):
     @staticmethod
     def is_displayed(player):
-        return player.group.end
+        return player.group.end_temp
 
 class PaymentInfo(Page):
     @staticmethod
@@ -152,7 +174,6 @@ class PaymentInfo(Page):
     def vars_for_template(player: Player):
         participant = player.participant
         return dict(redemption_code=participant.label or participant.code)
-
 
 
 page_sequence = [
